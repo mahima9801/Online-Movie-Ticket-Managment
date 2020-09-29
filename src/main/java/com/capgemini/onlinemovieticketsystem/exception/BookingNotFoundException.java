7c1063b0512da4e2c006db9715c1573a3dec08ea
mahima9801 <60997303+mahima9801@users.noreply.github.com>
2020-09-29 19:03:09 +0530

    Delete BookingNotFoundException.java

--- src/main/java/com/capgemini/onlinemovieticketsystem/exception/BookingNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.onlinemovieticketsystem.exception;
-
-public class BookingNotFoundException extends RuntimeException {
-
-	public BookingNotFoundException(String msg)
-	{
-		super(msg);
-	}
-}
