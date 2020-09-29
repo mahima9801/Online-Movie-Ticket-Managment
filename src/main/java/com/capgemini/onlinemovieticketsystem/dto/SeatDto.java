3b5799fd46d430423eeb4c21a8fbbdff62d27b1d
mahima9801 <60997303+mahima9801@users.noreply.github.com>
2020-09-29 18:55:31 +0530

    Update SeatDto.java

--- src/main/java/com/capgemini/onlinemovieticketsystem/dto/SeatDto.java
+++ src/main/java/com/capgemini/onlinemovieticketsystem/dto/SeatDto.java
@@ -1,34 +1,28 @@
 package com.capgemini.onlinemovieticketsystem.dto;
 
+import com.capgemini.onlinemovieticketsystem.entity.BookingStatus;
+
 public class SeatDto {
+	private int seatId;
+	private BookingStatus seatStatus;
+	private Integer seatPrice;
+	public int getSeatId() {
+		return seatId;
+	}
+	public void setSeatId(int seatId) {
+		this.seatId = seatId;
+	}
+	public BookingStatus getSeatStatus() {
+		return seatStatus;
+	}
+	public void setSeatStatus(BookingStatus seatStatus) {
+		this.seatStatus = seatStatus;
+	}
+	public Integer getSeatPrice() {
+		return seatPrice;
+	}
+	public void setSeatPrice(Integer seatPrice) {
+		this.seatPrice = seatPrice;
+	}
 
-
-        private int seatId;
-
-        private double seatPrice;
-
-        public int getSeatId() {
-            return seatId;
-        }
-
-        public void setSeatId(int seatId) {
-            this.seatId = seatId;
-        }
-
-        public double getSeatPrice() {
-            return seatPrice;
-        }
-
-        public void setSeatPrice(double seatPrice) {
-            this.seatPrice = seatPrice;
-        }
-
-        @Override
-        public String toString() {
-            return "SeatDto [seatId=" + seatId + ", price=" + seatPrice + "]";
-        }
-    }
-
-
-
-
+}
