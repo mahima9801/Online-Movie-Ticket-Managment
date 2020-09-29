6ead45a357bbaa565cb26f62b4850adf9f6b8194
mahima9801 <60997303+mahima9801@users.noreply.github.com>
2020-09-29 19:03:31 +0530

    Delete MovieNotFoundException.java

--- src/main/java/com/capgemini/onlinemovieticketsystem/exception/MovieNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.onlinemovieticketsystem.exception;
-
-public class MovieNotFoundException extends RuntimeException {
-
-	public MovieNotFoundException(String msg)
-	{
-	super(msg);
-        }
-	}
