e5fdd47485ea9e4d4f36f49eb6d5907ae5d4332b
mahima9801 <60997303+mahima9801@users.noreply.github.com>
2020-09-29 19:00:02 +0530

    Update CustomerServiceImple.java

--- src/main/java/com/capgemini/onlinemovieticketsystem/serviceimpl/CustomerServiceImple.java
+++ src/main/java/com/capgemini/onlinemovieticketsystem/serviceimpl/CustomerServiceImple.java
@@ -1,244 +1,125 @@
 package com.capgemini.onlinemovieticketsystem.serviceimpl;
 
-import com.capgemini.onlinemovieticketsystem.dao.*;
-import com.capgemini.onlinemovieticketsystem.entity.*;
-import com.capgemini.onlinemovieticketsystem.exception.*;
-import com.capgemini.onlinemovieticketsystem.service.CustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-public class CustomerServiceImple implements CustomerService {
+import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.capgemini.onlinemovieticketsystem.dao.SeatDao;
+import com.capgemini.onlinemovieticketsystem.dao.ShowDao;
+import com.capgemini.onlinemovieticketsystem.entity.BookingStatus;
+import com.capgemini.onlinemovieticketsystem.entity.Seat;
+import com.capgemini.onlinemovieticketsystem.entity.Show;
+import com.capgemini.onlinemovieticketsystem.exceptions.SeatNotFoundException;
+import com.capgemini.onlinemovieticketsystem.exceptions.ShowNotFoundException;
+import com.capgemini.onlinemovieticketsystem.service.CustomerService;
 
-    @Autowired
-    private MovieDao movieDao;
+@Service
+@Transactional
+public  class CustomerServiceImpl implements CustomerService{
+	@Autowired
+	private SeatDao seatDao;
+	
+	@Autowired
+	private ShowDao showDao;
 
-    @Autowired
-    private CustomerDao customerDao;
-
-    @Autowired
-    private SeatDao seatDao;
-
-    @Autowired
-    private ShowDao showDao;
-
-    @Autowired
-    private BookingDao bookingDao;
-
-    @Autowired
-    private TheaterDao theaterDao;
-
-    @Autowired
-    private TicketDao ticketDao;
-
-    @Autowired
-    private TransactionDao transactionDao;
-
-    @Autowired
-    private ScreenDao screenDao;
-
-
-    //-------------------------------------------BOOKING-----------------------------------//
-
-
-    @Override
-    public Booking addBooking(Booking booking) {
-        booking = bookingDao.save(booking);
-        return booking;
-    }
-
-    @Override
-    public Booking createBooking(Booking booking, String paymentMethod, String screenName) {
-        BookingTransaction bookingTransaction = makePayment(paymentMethod,booking.getTotalCost());
-        Ticket ticket = createTicket(booking.getSeatIds(),screenName);
-        booking.setBookingDate(LocalDate.now());
-        booking.setTransactionId(bookingTransaction.getTransactionId());
-        booking.setTicket(ticket);
-        booking = addBooking(booking);
-        return booking;
-    }
-
-
-    @Override
-    public Booking fetchBookingById(int bookingId) {
-        Optional<Booking> option= bookingDao.findById(bookingId);
-        if(!option.isPresent()) {
-            throw new BookingNotFoundException("Booking id is wrong. No booking exist with this booking id :"+bookingId);
-        }
-        Booking booking = option.get();
-        return booking;
-    }
-
-    @Override
-    public List<Booking> fetchAllBookings() {
-        List<Booking> bookingList = bookingDao.findAll();
-        return bookingList;
-    }
-
-    @Override
-    public String cancelBooking(int bookingId) {
-        Booking booking = fetchBookingById(bookingId);
-        Ticket ticket = booking.getTicket();
-        ticketDao.delete(ticket);
-        bookingDao.delete(booking);
-        return "Cancelled";
-    }
-
-    @Override
-    public BookingTransaction makePayment(String paymentMethod, double cost) {
-        BookingTransaction transaction = new BookingTransaction();
-        transaction.setTransactionAmount(cost);
-        transaction.setTransactionMethod(paymentMethod);
-        transaction = transactionDao.save(transaction);
-        return transaction;
-    }
-
-    //------------------------------------TICKET-------------------------------------//
-
-    /**
-     * generating ticket
-     * @param seatIds
-     * @param screenName
-     * @return
-     */
-    public Ticket createTicket(List<Integer> seatIds,String screenName) {
-        Ticket ticket= new Ticket();
-        ticket.setNoOfSeats(seatIds.size());
-        ticket.setScreenName(screenName);
-        ticket.setSeatIds(seatIds);
-        ticket.setTicketStatus(TicketStatus.BOOKED);
-        ticket = ticketDao.save(ticket);
-        return ticket;
-    }
-
-
-    @Override
-    public Ticket showTicket(int bookingId) {
-        Booking booking = fetchBookingById(bookingId);
-        Ticket ticket = booking.getTicket();
-        if(ticket==null) {
-            throw new TicketNotFoundException("No Ticket has booked yet.");
-        }
-        return ticket;
-    }
-
-    //---------------------------------------SCREEN---------------------------------------//
-
-    @Override
-    public List<Screen> fetchAllScreen() {
-        List<Screen> screenList = screenDao.findAll();
-        return screenList;
-    }
-
-    @Override
-    public Screen fetchScreenById(int screenId) {
-        Optional<Screen> optional = screenDao.findById(screenId);
-        if(optional.isPresent()) {
-            Screen screen = optional.get();
-            return screen;
-        }
-        throw new ScreenNotFoundException("Screen not found at id ="+screenId);
-    }
-
-    @Override
-    public List<Theater> fetchAllTheater() {
-        List<Theater> theaters = theaterDao.findAll();
-        return theaters;
-    }
-
-    @Override
-    public Theater fetchTheaterById(int theaterId) {
-        Optional<Theater> optional = theaterDao.findById(theaterId);
-        if(optional.isPresent()) {
-            Theater th = optional.get();
-            return th;
-        }
-        throw new TheaterNotFoundException("theater not found for id="+theaterId);
-    }
-
-
-    //---------------------------------------MOVIE----------------------------------//
-    @Override
-    public List<Movie> fetchAllMovie() {
-        List<Movie> movies = movieDao.findAll();
-        return movies;
-    }
-
-    @Override
-    public Movie fetchMovieById(int movieId) {
-        Optional<Movie> optional = movieDao.findById(movieId);
-        if (optional.isPresent()) {
-            Movie mov = optional.get();
-            return mov;
-        }
-        throw new MovieNotFoundException("Movie not found for id=" + movieId);
-    }
-
-
-    //----------------------------------------SHOW---------------------------------------//
-    @Override
-    public Show fetchShowById(int showId) {
-        Optional<Show> optional = showDao.findById(showId);
-        if(optional.isPresent()) {
-            Show show = optional.get();
-            return show;
-        }
-        throw new ShowNotFoundException("Show not found for id="+showId);
-
-    }
-
-    @Override
-    public List<Show> fetchAllShow() {
-        List<Show> shows = showDao.findAll();
-        return shows;
-    }
-
-    //------------------------------------------------SEAT----------------------------------//
-
-    @Override
-    public Seat addSeat(Seat seat) {
-        seatDao.save(seat);
-        return seat;
-    }
-    @Override
-    public Seat getSeat(int id) {
-        Optional<Seat> optional = seatDao.findById(id);
+	@Override
+	public Seat addSeat(Seat seat) {
+		seatDao.save(seat);
+		return seat;
+	}
+	
+	/*
+	 * getSeat method which will get the data of particular seat from Seat table through "seatId".
+	 */
+	
+	@Override
+	public Seat getSeat(int seatId) throws SeatNotFoundException {
+        Optional<Seat> optional = seatDao.findById(seatId);
         if(optional.isPresent())
         {
             Seat seat = optional.get();
-            return seat;
+           	return seat;
         }
-        throw new SeatNotFoundException("Seat not found for id:"+id);
+        	throw new SeatNotFoundException("Seat not found for id:"+seatId);
     }
-    @Override
-    public Seat blockSeat(int id) {
-        Seat seat = getSeat(id);
-        seat.setSeatStatus(SeatStatus.BLOCKED);
-        seatDao.save(seat);
-        return seat;
-    }
-    @Override
-    public Seat bookSeat(int id) {
-        Seat seat = getSeat(id);
-        seat.setSeatStatus(SeatStatus.BOOKED);
-        seatDao.save(seat);
-        return seat;
-    }
-    @Override
-    public Seat cancelSeat(int id) {
-        Seat seat = getSeat(id);
-        seat.setSeatStatus(SeatStatus.AVAILABLE);
-        seatDao.save(seat);
-        return seat;
-    }
+	
+	/*
+	 * blockSeat method which will change the status of the seat to "BLOCKED".
+	 */
+	
+	@Override
+	public Seat blockSeat(int seatId) throws SeatNotFoundException {
+      Seat seat = getSeat(seatId);
+      seat.setSeatStatus(BookingStatus.BLOCKED);
+      seatDao.save(seat);
+      return seat;
+  }
+	
+	/*
+	 * bookSeat method which will change the status of the seat to "BOOKED".
+	 */
+	@Override
+	public Seat bookSeat(int id) throws SeatNotFoundException {
+      Seat seat = getSeat(id);
+      seat.setSeatStatus(BookingStatus.BOOKED);
+      seatDao.save(seat);
+      return seat;
+  }
+	
+	/*
+	 * cancelSeat method which will change the status of the seat to "AVAILABLE".
+	 */
+	@Override
+	public Seat cancelSeat(int id) throws SeatNotFoundException {
+      Seat seat = getSeat(id);
+      seat.setSeatStatus(BookingStatus.AVAILABLE);
+      seatDao.save(seat);
+      return seat;
+  }
+	
+	/*
+	 * getAllSeats method which will fetch all the data stored in Seat table.
+	 */
+	@Override
+	public List<Seat> getAllSeats() {
+		 List<Seat> seat = seatDao.findAll();
+	     return seat;
+	}
 
-    @Override
-    public List<Seat> fetchAllSeats() {
-        List<Seat> list = seatDao.findAll();
-        return list;
-    }
+	//Show Module
+	@Override
+	public Show addShow(Show show) {
+		showDao.save(show);
+		return show;
+	}
+	
+	/*
+     * findById method will fetch the data of particular show from Show table through "showId".
+     */
 
+	@Override
+	public Show findById(int showId) throws ShowNotFoundException {
+		Optional<Show> optional = showDao.findById(showId);
+		if(optional.isPresent()) {
+			Show show = optional.get();
+			return show;
+		}
+		throw new ShowNotFoundException("Show not found for id:"+showId);
+	}
+	
+	/*
+     * fetchAllShows method will fetch all the shows stored in Show table.
+     */
+	@Override
+	public List<Show> fetchAllShows() {
+		 List<Show> shows = showDao.findAll();
+	     return shows;
+	}
+
+
+	
+	
 }
