499100e094e0ad975579238a12d08a7d9bb1ab82
mahima9801 <60997303+mahima9801@users.noreply.github.com>
2020-09-29 18:50:44 +0530

    Delete TransactionDao.java

--- src/main/java/com/capgemini/onlinemovieticketsystem/dao/TransactionDao.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.onlinemovieticketsystem.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capgemini.onlinemovieticketsystem.entity.BookingTransaction;
-
-public interface TransactionDao extends JpaRepository<BookingTransaction, Integer>{
-
-}
