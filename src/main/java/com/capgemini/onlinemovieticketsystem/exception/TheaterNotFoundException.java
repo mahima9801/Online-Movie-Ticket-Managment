d557e779980dd07e41269a3ca57fc98e6acbc853
mahima9801 <60997303+mahima9801@users.noreply.github.com>
2020-09-29 19:03:51 +0530

    Delete TheaterNotFoundException.java

--- src/main/java/com/capgemini/onlinemovieticketsystem/exception/TheaterNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.onlinemovieticketsystem.exception;
-
-public class TheaterNotFoundException extends RuntimeException{
-    public TheaterNotFoundException(String msg) {
-        super(msg);
-    }
-}
